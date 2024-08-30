5a6f4d9ba9202d16a93701fc47687fd81a56c441
Aleksandr Egert <9213855+kreigan@users.noreply.github.com>
2024-08-30 12:35:05 -0400

    export public objects

---
+++ src/dicontainer/__init__.py
@@ -0,0 +1,9 @@
+from .di import ServiceCollection as ServiceCollection
+from .di import ServiceLifetime as ServiceLifetime
+from .di import ServiceProvider as ServiceProvider
+
+__all__ = [
+    "ServiceCollection",
+    "ServiceLifetime",
+    "ServiceProvider",
+]
